0a7a9fc092afca6b7d7e21b8feb1c3f89bc38d47
Felipe Lima <lipezera1836@gmail.com>
2025-09-22 10:58:32 -0300

    feat(auth): user schema

--- models/user_model.py
+++ models/user_model.py
@@ -6,5 +6,6 @@ class User(Base):
     __tablename__ = "users"
 
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, nullable=False)
+    username = Column(String, unique=True, index=True, nullable=False)
     email = Column(String, unique=True, index=True, nullable=False)
     hashed_password = Column(String, nullable=False)
---
+++ schemas/user_schema.py
@@ -0,0 +1,11 @@
+import uuid
+from typing import Optional
+from pydantic import BaseModel, EmailStr
+
+class UserBase(BaseModel):
+    username: str
+    email: EmailStr
+
+
+class UserCreate(UserBase):
+    password: str
