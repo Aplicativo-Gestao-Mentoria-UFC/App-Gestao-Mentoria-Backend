8d1686184f3bdd23ff60f161e1a5432dfe3dcbae
Felipe Lima <lipezera1836@gmail.com>
2025-09-22 23:25:55 -0300

    feat(auth): implementada função que cria token jwt

--- core/config.py
+++ core/config.py
@@ -2,8 +2,13 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     API_STR: str
+    
     DB_URL: str
+
+    JWT_SECRET: str
     ALGORITHM: str
+
+    ACCESS_TOKEN_EXPIRE_MINUTES: int
 
     class Config:
         env_file = ".env"
--- core/security.py
+++ core/security.py
@@ -1,6 +1,7 @@
 from passlib.context import CryptContext
-
-
+from datetime import datetime, timezone, timedelta
+from jose import jwt
+from core.config import settings
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 def verify_password(plain_password: str, hashed_password: str):
@@ -8,3 +9,13 @@ def verify_password(plain_password: str, hashed_password: str):
 
 def get_password_hash(password: str):
     return pwd_context.hash(password)
+
+def create_acess_token(data: dict, expires_delta: timedelta | None = None):
+    to_encode = data.copy()
+    sp = timezone('America/Sao_Paulo')
+    if expires_delta:
+        expire = datetime.now(tz=sp) + expires_delta
+    else:
+        expire = datetime.now(tz=sp) + timedelta(minutes=15)
+    to_encode.update({"exp": expire, "iat": datetime.now(tz=sp)})
+    return jwt.encode(to_encode, settings.JWT_SECRET, algorithm=settings.ALGORITHM)
